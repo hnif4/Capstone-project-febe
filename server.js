fee2f5616138e1650edd01d42fa737e9875637ee
hnif4 <nhanifah320@gmail.com>
2025-04-16 15:48:08 +0700

    update commit

--- server.js
+++ server.js
@@ -15,7 +15,13 @@ const notificationRoutes = require('./routes/notificationRoutes')
 const mlPredictRoute = require('./routes/mlPredictRoutes')
 
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
+
+
+
+
+app.use(cors())
+
 
 app.use(express.json())
 app.use('/api', educontentRoutes)
