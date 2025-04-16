a38114b21aa613a59f2880793d051c392b66b26d
hnif4 <nhanifah320@gmail.com>
2025-04-16 15:42:04 +0700

    Initial commit

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,19 @@
-const mysql = require('mysql');
-require('dotenv').config();
+const mysql = require('mysql2')
+require('dotenv').config()
 
-const db = mysql.createPool({
-    connectionLimit: 10, // Max 10 koneksi yang dapat dibuka
-    host: process.env.DB_HOST,      // Gunakan variabel lingkungan untuk host
-    user: process.env.DB_USER,      // Gunakan variabel lingkungan untuk user
-    password: process.env.DB_PASS,  // Gunakan variabel lingkungan untuk password
-    database: process.env.DB_NAME   // Gunakan variabel lingkungan untuk nama database
-});
+const db = mysql.createConnection({
+    host: "sql.freedb.tech",
+    user: "freedb_hanifah",
+    password: "@y*vxS??!H#9&6D",
+    database: "freedb_db_trashtocash"
+})
 
-db.getConnection((err, connection) => {
+db.connect(err => {
     if (err) {
-        console.log("Database connection failed:", err);
+        console.log("Database connection failed:", err)
     } else {
-        console.log("Database connected!");
-        connection.release(); // Setelah koneksi berhasil, kita lepas untuk digunakan lagi
+        console.log("Database connected!")
     }
 });
 
-module.exports = db;
+module.exports = db
